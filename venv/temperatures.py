232652cbdcf9113346ee9568456a01196355fc66
Your Name <patrycja.bednarska@gmail.com>
2020-10-30 21:48:20 +0100

    temperatury

---
+++ venv/temperatures.py
@@ -0,0 +1,20 @@
+# zródło problemu : https://www.codingame.com/ide/puzzle/temperatures ; zadanie wykonane przez Patrycję Bednarską
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())
+temps = input()
+if n == 0:
+    result = 0
+    print(result)
+else:
+    min_temp = None
+    new_temps = [int(x) for x in temps.split()]
+    for temp in new_temps:
+        if (min_temp is None) or (abs(temp) < abs(min_temp)) or (temp == -min_temp) and (temp > 0):
+            min_temp = temp
+    print(min_temp)
